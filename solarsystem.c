cf99bf929b2107e5ae75743e2bf69b77a5eb8985
tadeu <tadeuscalvin63@gmail.com>
2019-01-26 17:26:31 -0200

    update

--- solarsystem.c
+++ solarsystem.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-	int N=11;
-	struct {
-		double x,y,z,vx,vy,vz,m;
-	} planet[N];
+#define N 11
 
+typedef struct {
+	double x,y,z,vx,vy,vz,m;
+} XYZ
+
+void init() {
 	FILE*f1=fopen("data.dat","r");
 	int i=0;
 	while(fscanf(f1,"%lf %lf %lf %lf %lf %lf %lf\n",&planet[i].vx,&planet[i].vy,&planet[i].vz,&planet[i].x,&planet[i].y,&planet[i].z,&planet[i].m)!=EOF) {
 		i++;
 	}
 	fclose(f1);
-	printf("%lf %lf",planet[5].x,planet[0].m);
+}
+
+int main() {
+	XYZ planet[N];
+	init();
 }
 
 //aceleracao() {
